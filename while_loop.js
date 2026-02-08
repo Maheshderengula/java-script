7e20d15a36238a9dac159f34af60093a5fce36f6
DERENGULA MAHESH <derengulamahesh7@gmail.com>
2026-02-08 17:55:07 +0530

    while-loop

---
+++ while_loop.js
@@ -0,0 +1,48 @@
+//basic while loop:
+let i = 1;
+
+while(i<=5) {
+    console.log (i);
+    i++;
+
+}
+//print even numbers:
+let g= 2;
+while(g<=20){
+    console.log(g);
+    g+=2;
+}
+
+//sum of numbers:
+let j =1;
+let sum =0;
+while (j <=10){
+    sum+=j;
+    g++;
+}
+console.log("sum:",sum);
+
+//loop through an array:
+let fruits =["apple","banana","mango"];
+let y =0;
+while (y<fruits.length){
+    console.log(fruits[i]);
+    i++;
+}
+
+//search element inn array :
+let nums = [10,20,30,40,]
+let m =0;
+let found=false;
+
+while(m< num.length){
+    if(nums[m]===30){
+        found=true;
+        break;
+    }
+    m++;
+
+}
+console.log(found ?"found":"not found");
+
+
