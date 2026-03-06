068a7bd26534665fe5a100e2d2ecf9f80bc9e816
DERENGULA MAHESH <derengulamahesh7@gmail.com>
2026-03-06 17:41:04 +0530

    ES6 new features

---
+++ product 3.js
@@ -0,0 +1,5 @@
+let  enames=['rammu','priya','mahii','narasimha','jevvi']
+//let new_enames=enames.map((ename)=>{return ename.touppercase()})
+let new_enames=enames.map(name=>name.toUpperCase())
+console.log(enames)
+console.log(new_enames)
---
+++ product.js
@@ -0,0 +1,8 @@
+let product_prices=[98,198,298,398,498]
+//create new array by adding +1
+let new_prices=[]
+for (let price of product_prices){
+    new_prices.push(price+1)
+}
+console.log(product_prices);
+console.log(new_prices);
