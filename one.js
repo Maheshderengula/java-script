c4c932da6a0accff74e15fd3cb071dc75f1edecb
DERENGULA MAHESH <derengulamahesh7@gmail.com>
2026-03-06 17:39:16 +0530

    opertors

--- one.js
+++ one.js
@@ -1,5 +1,28 @@
-function add(){
-
-    console.log("Addition")
+class account{
+    acc_Id;
+    acc_Name;
+    acc_Bal;
+    min_Bal=500;
+    constructor(id, Name,amount){
+        this.acc_Id =id;
+        this.acc_Name = Name;
+        this.acc_Bal= amount;
+    }
+    deposit_Amount(){}
+    withdrawl(){}
+    get_Bal(){}
 }
-add()
+let a1=new Account()
+a1.deposit_Amount(500)
+a1.deposit_Amount(1500)
+a1.deposit_Amount(2500)
+let a2=new Account()
+a2.deposit_Amount(50)
+a2.withdrawl(10)
+let a3=new Account()
+console.log(a1)
+console.log(a2)
+console.log(a3)
+console.log(a1.get_Bal())
+console.log(a2.get_Bal())
+console.log(a3.get_Bal())
--- two.js
+++ two.js
@@ -1,7 +1,24 @@
-let a=100;
-let add=()=>{
-            console.log("Addition")
-        }
-console.log(typeof a)    //number   
-console.log(typeof add)  //function
-add()  //invoking function
+class Account{
+    acc_Id;
+    acc_Name;
+    acc_Bal;
+    min_Bal=500;
+    constructor(id,name,amount){
+        this.acc_Id=id;
+        this.acc_Name=name;
+        this.acc_Bal=amount;
+    }
+    deposit_Amount(amount){
+        this.acc_Bal=this.acc_Bal+amount;
+    }
+
+}
+let a1=new Account(101,"RG",5000)
+let a2=new Account(102,"SG",15000)
+console.log(a1)
+console.log(a2)
+a1.deposit_Amount(500)
+a1.deposit_Amount(50)
+a2.deposit_Amount(6000)
+console.log(a1)
+console.log(a2)
